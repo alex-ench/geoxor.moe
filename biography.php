bb980fff83ee045995ad15b41c3e9758d74469ea
SHISEN <alex-ench@yandex.ru>
2021-05-29 18:08:46 +0400

    Biography page

---
+++ biography.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Geoxor | Biography</title>
+    <link rel="icon" href="./img/favicon.ico">
+    <script src="https://kit.fontawesome.com/4abd74a538.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="./css/General.css"/>
+    <link rel="stylesheet" type="text/css" href="./css/Biography.css"/>
+</head>
+<body>
+<div id="app">
+    <header>
+        <div class="wrap"><a href="index.php" class="router-link-active router-link-exact-active" aria-current="page">
+                <img class="logo" src="./img/logo.3bc4e30a.svg" alt="logo"></a>
+            <nav>
+                <ul class="nav-links">
+                    <li><a href="tours.php" class="">Tours</a></li>
+                    <li><a href="faq.php" class="">FAQ</a></li>
+                    <li><a href="biography.php" class="">Biography</a></li>
+                    <li><a href="contact.php" class="">Contact</a></li>
+                    <li>
+                        <div class="btn"><a href="releases.php" class=""><span><i class="fas fa-download"></i> Releases</span></a>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <div id="biography" class="biography">
+        <h1 class="biography-title">Biography</h1>
+        <div class="biography-container"><p class="biography-content">George Tsotsos (born 27
+                March 2001), better known as Geoxor (commonly misspelled as Geoxer), is a Greek music producer from
+                Florina,
+                Greece.
+                <br><br> As an independent producer Geoxor is influenced by Panda Eyes, Virtual
+                Riot, Porter Robinson, Skrillex, and many others, along with having an anime theme throughout his
+                artwork
+                <br
+                ><br> In 2016 NoCopyrightSounds signed his single “You &amp; I” and has
+                surpassed 10 million views on YouTube. <br><br> In 2017 he released multiple singles
+                along with his first EP called “Funky World” featuring 3 tunes one of which DSG promoted on YouTube. <br
+                ><br> In 2018 he released his second EP called “Time of Waste” as a pun
+                directed towards his real life events that were happening as he was making the EP. <br><br
+                > In 2019 he started off the year by releasing “Stardust” a catchy video game style tune,
+                which DSG had promoted on his YouTube, getting up to 1+ million views within 3 months, further more
+                having
+                Knife
+                Party’s support @ ULTRA 2019 with the tune “Nana”</p></div>
+    </div>
+</div>
+</body>
+</html>
