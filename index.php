02cb5c83b8224fa4ea5bad9a40ef31044c1d3101
SHISEN <alex-ench@yandex.ru>
2021-05-29 18:07:27 +0400

    Main page

---
+++ index.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Geoxor | Home</title>
+    <link rel="icon" href="./img/favicon.ico">
+    <script src="https://kit.fontawesome.com/4abd74a538.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="./css/General.css"/>
+    <link rel="stylesheet" type="text/css" href="./css/Home.css"/>
+</head>
+<body>
+<div id="app">
+    <header>
+        <div class="wrap"><a href="index.php" class="router-link-active router-link-exact-active"
+                             aria-current="page"><img class="logo" src="./img/logo.3bc4e30a.svg" alt="logo"></a>
+            <nav>
+                <ul class="nav-links">
+                    <li><a href="tours.php" class="">Tours</a></li>
+                    <li><a href="faq.php" class="">FAQ</a></li>
+                    <li><a href="biography.php" class="">Biography</a></li>
+                    <li><a href="contact.php" class="">Contact</a></li>
+                    <li>
+                        <div class="btn"><a href="releases.php" class=""><span><i class="fas fa-download"></i> Releases</span></a>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <section class="welcome">
+        <div class="welcome-box">
+            <div class="welcome-box-content">
+                <div class="welcome-box-logo"></div>
+                <h1>WELCOME</h1>
+                <p>Hi, I'm Geoxor. I'm a 19 year old EDM producer from Greece and I've been
+                    making music since 2012. Welcome to my webpage where you can see all my work and download it!
+                    <br><br> If you want to know more about me go check out
+                    the FAQ section of my page!</p>
+            </div>
+        </div>
+    </section>
+</div>
+</body>
+</html>
